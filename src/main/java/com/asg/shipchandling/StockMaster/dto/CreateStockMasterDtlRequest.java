cd99237d29efae32286f599bb828790c5bd26c23
Tharani <127190199+Tharani16@users.noreply.github.com>
2025-12-04 21:00:52 +0530

    ChangesFix

--- src/main/java/com/asg/shipchandling/StockMaster/dto/CreateStockMasterDtlRequest.java
+++ src/main/java/com/asg/shipchandling/StockMaster/dto/CreateStockMasterDtlRequest.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateStockMasterDtlRequest {
+    private Long detRowId;  // Used to identify existing records for update/delete/noChange
     private Long supplierPoid;
     private String supplierStockCode;
     private String remarks;
