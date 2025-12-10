ff40c03b353a4acfa6db440ed7b2aa79fe7e509d
mjainhexa <mjain@hexalytics.com>
2025-12-10 13:11:31 +0530

    Fixed build issue

--- src/main/java/com/asg/shipchandling/ShipchandlingApplication.java
+++ src/main/java/com/asg/shipchandling/ShipchandlingApplication.java
@@ -2,10 +2,14 @@ package com.asg.shipchandling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.asg"})
+@EnableJpaRepositories(basePackages = {"com.asg.common.lib.repository", "com.asg.shipchandling"})
+@EntityScan(basePackages = {"com.asg.common.lib.entity", "com.asg.shipchandling"})
 public class ShipchandlingApplication {
 
 	public static void main(String[] args) {
