c6470c4f819fc7c7eedb1865b5c9a7ed3dcf496e
Muruga-Hexalytics <mpalaniraja@hexalytics.com>
2025-12-17 18:22:46 +0530

    Enhance StockMaster DTOs with validation annotations
    - Added validation annotations to CreateStockMasterDtlRequest, CreateStockMasterRequest, and UpdateStockMasterRequest to enforce required fields and character limits.
    - Updated size constraints for stock name and description fields to improve data integrity.
    - Introduced new validation messages for better user feedback on required fields.

--- src/main/java/com/asg/shipchandling/StockMaster/dto/CreateStockMasterDtlRequest.java
+++ src/main/java/com/asg/shipchandling/StockMaster/dto/CreateStockMasterDtlRequest.java
@@ -1,5 +1,7 @@
 package com.asg.shipchandling.StockMaster.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +11,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CreateStockMasterDtlRequest {
     private Long detRowId;  // Used to identify existing records for update/delete/noChange
+    @NotNull(message = "Supplier is required")
     private Long supplierPoid;
+    @NotBlank(message = "Supplier stock code is required")
     private String supplierStockCode;
     private String remarks;
     private String actionType;
