893002c14f263dc30033e037cd2e3a1df591fcf4
Muruga-Hexalytics <mpalaniraja@hexalytics.com>
2026-01-20 16:28:12 +0530

    Refactor StockMasterServiceImpl and SalesQuotationSchServiceImpl for improved error handling and data retrieval
    - Updated StockMasterServiceImpl to return an empty response instead of throwing an exception when no stock details are found, preventing transaction rollbacks.
    - Enhanced SalesQuotationSchServiceImpl to handle exceptions during DTO conversion and logging without causing transaction rollbacks.
    - Refactored repository methods to streamline data retrieval for sales quotations and customer details, ensuring better separation of concerns and improved error handling.

--- src/main/java/com/asg/shipchandling/salesquotationsch/repository/SalesQuotationSchHdrRepository.java
+++ src/main/java/com/asg/shipchandling/salesquotationsch/repository/SalesQuotationSchHdrRepository.java
@@ -59,68 +59,105 @@ public interface SalesQuotationSchHdrRepository extends JpaRepository<SalesQuota
                         Long companyPoid, String deleted, Pageable pageable);
 
         /**
-         * Get Sales Quotation SCH with all LOV details in a single query using JOINs
-         * Returns Object[] with quotation data and all related LOV details
+         * Get Sales Quotation SCH header data only (without LOV details)
+         * Returns Object[] with quotation header fields
          */
         @Query(value = "SELECT " +
-                        "qtn.TRANSACTION_POID as transactionPoid, qtn.DOC_REF as docRef, qtn.TRANSACTION_DATE as transactionDate, "
-                        +
-                        "qtn.COMPANY_POID as companyPoid, qtn.CUSTOMER_POID as customerPoid, qtn.ADDRESS_POID as addressPoid, "
-                        +
-                        "qtn.CURRENCY_CODE as currencyCode, qtn.CURRENCY_RATE as currencyRate, " +
-                        "qtn.QUOTATION_STATUS as quotationStatus, qtn.SALESMAN_POID as salesmanPoid, " +
-                        "qtn.VALIDITY_FROM_DATE as validityFromDate, qtn.VALIDITY_TO_DATE as validityToDate, " +
-                        "qtn.PAYMENT_MODE as paymentMode, qtn.DELIVERY_TERMS as deliveryTerms, " +
-                        "qtn.LINE_POID as linePoid, qtn.VESSEL_POID as vesselPoid, qtn.VESSEL_NAME as vesselName, " +
-                        "qtn.VOYAGE_REF as voyageRef, qtn.PORT_POID as portPoid, qtn.PORT_DESCRIPTION as portDescription, "
-                        +
-                        "qtn.REMARKS as remarks, qtn.TOTAL_DISCOUNT as totalDiscount, qtn.TOTAL_AMOUNT as totalAmount, "
-                        +
-                        "qtn.ACTION_STATUS as actionStatus, qtn.ACTION_DUE_DATE as actionDueDate, " +
-                        "qtn.ENQUIRY_REF_NUMBER as enquiryRefNumber, qtn.LOST_REASON as lostReason, " +
-                        "qtn.BUSINESS_PROMOTION_VALUE as businessPromotionValue, qtn.PERCENTAGE as percentage, " +
-                        "qtn.DETAILS as details, qtn.EXPEACTED_DELIVERY_DATE as expectedDeliveryDate, " +
-                        "qtn.VESSEL_AGENT as vesselAgent, qtn.QUOTED_RATE as quotedRate, qtn.RFQ_REF_NO as rfqRefNo, " +
-                        "qtn.PERCENTAGE_DISC as percentageDisc, qtn.TOTAL_GP_AMT as totalGpAmt, " +
-                        "qtn.TOTAL_GP_PERCENTAGE as totalGpPercentage, qtn.CUSTOMER_REF as customerRef, " +
-                        "qtn.DELIVERY_TO_ADDRESS as deliveryToAddress, qtn.SELECT_ALL_DTL as selectAllDtl, " +
-                        "qtn.TOT_AMT_PRINT_YN as totAmtPrintYn, qtn.DESCRIPTION_PRINT_YN as descriptionPrintYn, " +
-                        "qtn.ADVANCE_DETAIL as advanceDetail, qtn.MTA_INV_POID as mtaInvPoid, " +
-                        "qtn.SALES_INV_POID as salesInvPoid, qtn.SALES_INV_DOC_REF as salesInvDocRef, " +
-                        "qtn.TOTAL_TAX as totalTax, qtn.PARTY_ADDRESS_DETAILS as partyAddressDetails, " +
-                        "qtn.DELETED as deleted, qtn.CREATED_BY as createdBy, qtn.CREATED_DATE as createdDate, " +
-                        "qtn.LASTMODIFIED_BY as lastmodifiedBy, qtn.LASTMODIFIED_DATE as lastmodifiedDate, " +
-                        // Customer Details
-                        "cust.CUSTOMER_POID as custPoid, cust.CUSTOMER_CODE as custCode, cust.CUSTOMER_NAME as custName, "
-                        +
-                        // Salesman Details
-                        "sm.SALESMAN_POID as smPoid, sm.SALESMAN_CODE as smCode, sm.SALESMAN_NAME as smName, " +
-                        // Line Details
-                        "lm.LINE_POID as lmPoid, lm.LINE_CODE as lmCode, lm.LINE_NAME as lmName, " +
-                        // Port Details
-                        "pm.PORT_POID as pmPoid, pm.PORT_CODE as pmCode, pm.PORT_NAME as pmName, " +
-                        // Vessel Details
-                        "vm.VESSEL_POID as vmPoid, vm.VESSEL_CODE as vmCode, vm.VESSEL_NAME as vmName, " +
-                        // Print Division Details (from GLOBAL_COMPANY_MASTER_DIV_DTL) - NULL since
-                        // field doesn't exist in table
-                        "NULL as divPoid, NULL as divCode, NULL as divDescription, " +
-                        // Principal Details - NULL since field doesn't exist in table
-                        "NULL as prPoid, NULL as prCode, NULL as prName, " +
-                        // Address Details
-                        // NOTE: SALES_QUOTATION_HDR.ADDRESS_POID stores ADDRESS_MASTER_POID (not GLOBAL_ADDRESS_DETAILS.ADDRESS_POID).
-                        // GLOBAL_ADDRESS_DETAILS may have multiple rows per master; pick one row (rn=1).
-                        "qtn.ADDRESS_POID as addrPoid, addr.CONTACT_PERSON as addrContactPerson, " +
-                        "addr.EMAIL1 as addrEmail1, addr.MOBILE as addrMobile " +
+                        "qtn.TRANSACTION_POID, qtn.DOC_REF, qtn.TRANSACTION_DATE, " +
+                        "qtn.COMPANY_POID, qtn.CUSTOMER_POID, qtn.ADDRESS_POID, " +
+                        "qtn.CURRENCY_CODE, qtn.CURRENCY_RATE, " +
+                        "qtn.QUOTATION_STATUS, qtn.SALESMAN_POID, " +
+                        "qtn.VALIDITY_FROM_DATE, qtn.VALIDITY_TO_DATE, " +
+                        "qtn.PAYMENT_MODE, qtn.DELIVERY_TERMS, " +
+                        "qtn.LINE_POID, qtn.VESSEL_POID, qtn.VESSEL_NAME, " +
+                        "qtn.VOYAGE_REF, qtn.PORT_POID, qtn.PORT_DESCRIPTION, " +
+                        "qtn.REMARKS, qtn.TOTAL_DISCOUNT, qtn.TOTAL_AMOUNT, " +
+                        "qtn.ACTION_STATUS, qtn.ACTION_DUE_DATE, " +
+                        "qtn.ENQUIRY_REF_NUMBER, qtn.LOST_REASON, " +
+                        "qtn.BUSINESS_PROMOTION_VALUE, qtn.PERCENTAGE, " +
+                        "qtn.DETAILS, qtn.EXPEACTED_DELIVERY_DATE, " +
+                        "qtn.VESSEL_AGENT, qtn.QUOTED_RATE, qtn.RFQ_REF_NO, " +
+                        "qtn.PERCENTAGE_DISC, qtn.TOTAL_GP_AMT, " +
+                        "qtn.TOTAL_GP_PERCENTAGE, qtn.CUSTOMER_REF, " +
+                        "qtn.DELIVERY_TO_ADDRESS, qtn.SELECT_ALL_DTL, " +
+                        "qtn.TOT_AMT_PRINT_YN, qtn.DESCRIPTION_PRINT_YN, " +
+                        "qtn.ADVANCE_DETAIL, qtn.MTA_INV_POID, " +
+                        "qtn.SALES_INV_POID, qtn.SALES_INV_DOC_REF, " +
+                        "qtn.TOTAL_TAX, qtn.PARTY_ADDRESS_DETAILS, " +
+                        "qtn.DELETED, qtn.CREATED_BY, qtn.CREATED_DATE, " +
+                        "qtn.LASTMODIFIED_BY, qtn.LASTMODIFIED_DATE " +
                         "FROM SALES_QUOTATION_HDR qtn " +
-                        "LEFT JOIN SALES_CUSTOMER_MASTER cust ON qtn.CUSTOMER_POID = cust.CUSTOMER_POID " +
-                        "LEFT JOIN SALES_SALESMAN_MASTER sm ON qtn.SALESMAN_POID = sm.SALESMAN_POID " +
-                        "LEFT JOIN SHIP_LINE_MASTER lm ON qtn.LINE_POID = lm.LINE_POID " +
-                        "LEFT JOIN SHIP_PORT_MASTER pm ON qtn.PORT_POID = pm.PORT_POID " +
-                        "LEFT JOIN SHIP_VESSEL_MASTER vm ON qtn.VESSEL_POID = TO_CHAR(vm.VESSEL_POID) " +
-                        "LEFT JOIN ( " +
-                        "  SELECT address_master_poid, contact_person, email1, mobile " +
-                        "  FROM ( " +
-                        "    SELECT d.*, ROW_NUMBER() OVER ( " +
+                        "WHERE qtn.TRANSACTION_POID = :transactionPoid AND qtn.COMPANY_POID = :companyPoid", nativeQuery = true)
+        List<Object[]> findSalesQuotationSchHeader(@Param("transactionPoid") Long transactionPoid,
+                        @Param("companyPoid") Long companyPoid);
+
+        /**
+         * Get Customer Details LOV (using ADDRESS_MASTER)
+         * Returns Object[] with [ADDRESS_MASTER_POID, ADDRESS_MASTER_POID, ADDRESS_NAME]
+         */
+        @Query(value = "SELECT am.ADDRESS_MASTER_POID, am.ADDRESS_MASTER_POID, am.ADDRESS_NAME " +
+                        "FROM GLOBAL_ADDRESS_MASTER am " +
+                        "WHERE am.ADDRESS_MASTER_POID = :addressPoid " +
+                        "AND (am.ACTIVE = 'Y' OR am.ACTIVE IS NULL) " +
+                        "AND (am.DELETED = 'N' OR am.DELETED IS NULL)", nativeQuery = true)
+        List<Object[]> findCustomerDetailsLov(@Param("addressPoid") Long addressPoid);
+
+        /**
+         * Get Customer Details LOV by CUSTOMER_POID (using ADDRESS_MASTER)
+         * Returns Object[] with [ADDRESS_MASTER_POID, ADDRESS_MASTER_POID, ADDRESS_NAME]
+         */
+        @Query(value = "SELECT am.ADDRESS_MASTER_POID, am.ADDRESS_MASTER_POID, am.ADDRESS_NAME " +
+                        "FROM GLOBAL_ADDRESS_MASTER am " +
+                        "WHERE am.ADDRESS_MASTER_POID = :customerPoid " +
+                        "AND NVL(am.ACTIVE, 'Y') = 'Y' " +
+                        "AND NVL(am.DELETED, 'N') = 'N'", nativeQuery = true)
+        List<Object[]> findCustomerDetailsLovByCustomerPoid(@Param("customerPoid") Long customerPoid);
+
+        /**
+         * Get Salesman Details LOV
+         * Returns Object[] with [SALESMAN_POID, SALESMAN_CODE, SALESMAN_NAME]
+         */
+        @Query(value = "SELECT sm.SALESMAN_POID, sm.SALESMAN_CODE, sm.SALESMAN_NAME " +
+                        "FROM SALES_SALESMAN_MASTER sm " +
+                        "WHERE sm.SALESMAN_POID = :salesmanPoid", nativeQuery = true)
+        List<Object[]> findSalesmanDetailsLov(@Param("salesmanPoid") Long salesmanPoid);
+
+        /**
+         * Get Line Details LOV
+         * Returns Object[] with [LINE_POID, LINE_CODE, LINE_NAME]
+         */
+        @Query(value = "SELECT lm.LINE_POID, lm.LINE_CODE, lm.LINE_NAME " +
+                        "FROM SHIP_LINE_MASTER lm " +
+                        "WHERE lm.LINE_POID = :linePoid", nativeQuery = true)
+        List<Object[]> findLineDetailsLov(@Param("linePoid") Long linePoid);
+
+        /**
+         * Get Port Details LOV
+         * Returns Object[] with [PORT_POID, PORT_CODE, PORT_NAME]
+         */
+        @Query(value = "SELECT pm.PORT_POID, pm.PORT_CODE, pm.PORT_NAME " +
+                        "FROM SHIP_PORT_MASTER pm " +
+                        "WHERE pm.PORT_POID = :portPoid", nativeQuery = true)
+        List<Object[]> findPortDetailsLov(@Param("portPoid") Long portPoid);
+
+        /**
+         * Get Vessel Details LOV
+         * Returns Object[] with [VESSEL_POID, VESSEL_CODE, VESSEL_NAME]
+         */
+        @Query(value = "SELECT vm.VESSEL_POID, vm.VESSEL_CODE, vm.VESSEL_NAME " +
+                        "FROM SHIP_VESSEL_MASTER vm " +
+                        "WHERE TO_CHAR(vm.VESSEL_POID) = :vesselPoid", nativeQuery = true)
+        List<Object[]> findVesselDetailsLov(@Param("vesselPoid") String vesselPoid);
+
+        /**
+         * Get Address Details (contact person, email, mobile)
+         * Returns Object[] with [ADDRESS_POID, CONTACT_PERSON, EMAIL1, MOBILE]
+         * NOTE: SALES_QUOTATION_HDR.ADDRESS_POID stores ADDRESS_MASTER_POID
+         */
+        @Query(value = "SELECT address_master_poid, contact_person, email1, mobile " +
+                        "FROM ( " +
+                        "  SELECT d.address_master_poid, d.contact_person, d.email1, d.mobile, " +
+                        "    ROW_NUMBER() OVER ( " +
                         "      PARTITION BY d.address_master_poid " +
                         "      ORDER BY " +
                         "        CASE WHEN UPPER(d.address_type) = 'SALES' THEN 0 ELSE 1 END, " +
@@ -129,13 +166,11 @@ public interface SalesQuotationSchHdrRepository extends JpaRepository<SalesQuota
                         "        CASE WHEN d.mobile IS NOT NULL AND TRIM(d.mobile) IS NOT NULL THEN 0 ELSE 1 END, " +
                         "        d.address_poid " +
                         "    ) rn " +
-                        "    FROM global_address_details d " +
-                        "  ) " +
-                        "  WHERE rn = 1 " +
-                        ") addr ON qtn.ADDRESS_POID = addr.address_master_poid " +
-                        "WHERE qtn.TRANSACTION_POID = :transactionPoid AND qtn.COMPANY_POID = :companyPoid", nativeQuery = true)
-        List<Object[]> findSalesQuotationSchWithDetails(@Param("transactionPoid") Long transactionPoid,
-                        @Param("companyPoid") Long companyPoid);
+                        "  FROM global_address_details d " +
+                        "  WHERE d.address_master_poid = :addressPoid " +
+                        ") " +
+                        "WHERE rn = 1", nativeQuery = true)
+        List<Object[]> findAddressDetails(@Param("addressPoid") Long addressPoid);
 
         /**
          * Find customer POIDs by customer name pattern
